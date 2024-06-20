13cf07c1c712fbdb248b53c04cc8982ce20c6922
mmasichuks <mmasi4jesus@gmail.com>
2024-06-20 13:36:13 +0100

    next commit

--- product/models.py
+++ product/models.py
@@ -1,22 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.utils.translation import  gettext_lazy as _
+from django.utils.text import slugify
 
 # Create your models here.
 
-class category(models.Model):
-     name = models.CharField( max_length=150)
-     slug = models.SlugField()
-     created = models.DateTimeField( auto_now=False, auto_now_add=False)
+class Category(models.Model):
+    name = models.CharField( max_length=150)
+    slug = models.SlugField()
+    created = models.DateTimeField( auto_now=False, auto_now_add=False)
+
+    class Meta:
+          verbose_name = 'Category'
+          verbose_name_plural ='Categories'
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f' categories for {self.name}'
+ 
+       
+
+
     
 class Product(models.Model):
     name = models.CharField( max_length=150)
     slug = models.SlugField()
-    category = models.ForeignKey("product.category", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey("product.Category",null =True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User,verbose_name=_("User"), on_delete=models.CASCADE)
     date = models.DateField()
     description = models.TextField()
     price = models.FloatField()
+    image =models.ImageField( upload_to='media',null=True ,blank=True)
+
+    def __str__(self):
+        return f'{self.category} || {self.name}'
 
     
 
