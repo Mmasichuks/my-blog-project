114e5356f35d5bcab811d2d1aeb067760f6778ea
mmasichuks <mmasi4jesus@gmail.com>
2024-06-20 14:04:47 +0100

    env

--- product/models.py
+++ product/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f' categories for {self.name}'
